64d2bbb4569817f6e51ed39200a3fa4b04a8d0cb
vekron <varun.kashyap1207@gmail.com>
2024-01-06 13:55:18 +0530

    Setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Styles, styles } from "../styles";
+import { navlinks } from "../constants";
+import { logo, menu, close } from "../assets";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+export default Navbar;
