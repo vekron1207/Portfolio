75e91a385b2d6dd2d2e7a0332a39114129dff48c
vekron <varun.kashyap1207@gmail.com>
2024-01-09 21:49:48 +0530

    Starting About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+
 import {
   About,
   Contact,
@@ -10,10 +11,11 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
+
 const App = () => {
   return (
     <BrowserRouter>
-      <div classname="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
